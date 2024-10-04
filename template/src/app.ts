96056e3e6189f157f0913c329cda861af59e8df6
mehrabix <mehrabi@post.com>
2024-10-04 13:44:31 +0330

    1.0.17: fix router

--- template/src/app.ts
+++ template/src/app.ts
@@ -3,39 +3,13 @@ import './global.css';
 
 const router = new Router();
 
-const handleRouterUpdate = async (newRouter: new () => Router) => {
-    console.log("Router module updated!");
-    const updatedRouter = new newRouter();
-    await updatedRouter.init();
-};
-
-if (module.hot) {
-  console.log('HMR enabled.');
-
-  module.hot.accept('./router', async () => {
-      console.log('Attempting to update router module...');
-      try {
-          const newRouterModule = (await import('./router')).default;
-          console.log('Router module updated:', newRouterModule);
-          await handleRouterUpdate(newRouterModule);
-      } catch (error) {
-          console.error('Error updating router module:', error);
-      }
-  });
-
-  module.hot.accept('./global.css', () => {
-      console.log("Global CSS updated!");
-  });
-}
-
-
 document.body.addEventListener('click', (event) => {
-    if (event.target instanceof HTMLAnchorElement) {
-        event.preventDefault();
-        const path = event.target.getAttribute('href')!;
-        history.pushState({}, '', path);
-        router.navigate(path);
-    }
+  if (event.target instanceof HTMLAnchorElement) {
+    event.preventDefault();
+    const path = event.target.getAttribute('href')!;
+    history.pushState({}, '', path);
+    router.navigate(path);
+  }
 });
 
 router.init();
