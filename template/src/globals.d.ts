21d3f1c48b82b790764edab78e76d1e824292087
mehrabix <mehrabi@post.com>
2024-10-04 16:15:10 +0330

    chore: remove global.d.ts

--- template/src/globals.d.ts
+++
@@ -1,9 +0,0 @@
-declare global {
-  interface NodeModule {
-    hot?: {
-      accept(dependencies?: string | string[], callback?: (updatedModule?: any) => void): void;
-    };
-  }
-}
-
-export {};
