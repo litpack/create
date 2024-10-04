1f30252ab0b3e1b376a36fc3d3fc5786d0d08396
mehrabix <mehrabi@post.com>
2024-10-04 12:12:05 +0330

    1.0.11

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 const { execSync } = require('child_process');
 const inquirer = require('inquirer');
@@ -14,7 +14,6 @@ if (!projectName) {
 
 const validPackageManagers = ['npm', 'yarn', 'pnpm', 'bun'];
 
-// Ask the user to select a package manager
 inquirer
   .prompt([
     {
@@ -24,68 +23,38 @@ inquirer
       choices: validPackageManagers,
     },
   ])
-  .then((answers) => {
+  .then(async (answers) => {
     const packageManager = answers.packageManager;
     const templateDir = path.join(__dirname, 'template');
     const targetDir = path.join(process.cwd(), projectName);
 
-    fs.mkdir(targetDir, { recursive: true }, (err) => {
-      if (err) {
-        console.error(`Error creating project directory: ${err}`);
-        process.exit(1);
-      }
-
-      copyTemplate(templateDir, targetDir, packageManager);
-    });
-  });
-
-function copyTemplate(src, dest, packageManager) {
-  fs.readdir(src, (err, items) => {
-    if (err) {
-      console.error(`Error reading template directory: ${err}`);
+    try {
+      await fs.mkdir(targetDir, { recursive: true });
+      await copyTemplate(templateDir, targetDir);
+      projectCreated(packageManager);
+    } catch (err) {
+      console.error(`Error creating project directory: ${err}`);
       process.exit(1);
     }
-
-    let copyCount = items.length;
-
-    items.forEach((item) => {
-      const srcPath = path.join(src, item);
-      const destPath = path.join(dest, item);
-
-      fs.stat(srcPath, (err, stats) => {
-        if (err) {
-          console.error(`Error getting stats for ${srcPath}: ${err}`);
-          process.exit(1);
-        }
-
-        if (stats.isDirectory()) {
-          fs.mkdir(destPath, { recursive: true }, (err) => {
-            if (err) {
-              console.error(`Error creating directory ${destPath}: ${err}`);
-              process.exit(1);
-            }
-            copyTemplate(srcPath, destPath, packageManager);
-            copyCount -= 1;
-            if (copyCount === 0) {
-              projectCreated(packageManager);
-            }
-          });
-        } else {
-          fs.copyFile(srcPath, destPath, (err) => {
-            if (err) {
-              console.error(`Error copying file ${item}: ${err}`);
-            } else {
-              console.log(`Copied ${item} to ${dest}`);
-            }
-            copyCount -= 1;
-            if (copyCount === 0) {
-              projectCreated(packageManager);
-            }
-          });
-        }
-      });
-    });
   });
+
+async function copyTemplate(src, dest) {
+  const items = await fs.readdir(src);
+  
+  await Promise.all(items.map(async (item) => {
+    const srcPath = path.join(src, item);
+    const destPath = path.join(dest, item);
+
+    const stats = await fs.stat(srcPath);
+
+    if (stats.isDirectory()) {
+      await fs.mkdir(destPath, { recursive: true });
+      await copyTemplate(srcPath, destPath);
+    } else {
+      await fs.copyFile(srcPath, destPath);
+      console.log(`Copied ${item} to ${destPath}`);
+    }
+  }));
 }
 
 function projectCreated(packageManager) {
